4844df1c9798497ed4e73d1f6d14b156553eae3f
Stefano Morgia <ste.morgia@gmail.com>
2022-10-05 15:29:15 +0200

    esercizio completato

--- laravel-base-crud/resources/views/fumetti/create.blade.php
+++ laravel-base-crud/resources/views/fumetti/create.blade.php
@@ -4,43 +4,78 @@
 
 @section('content')
 
-    <form action="{{route()}}" method="POST">
+    <form action="{{route(fumetti.store)}}" method="POST">
 
         @csrf
 
         <div>
             <label for="title">Titolo</label>
-            <input type="text" id="title" name="title">
+            <input type="text" id="title" name="title" value="{{old('title')}}">
+            @error('title')
+                <div>
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div>
             <label for="description">Descrizione</label>
-            <input type="text" id="description" name="description">
+            <input type="text" id="description" name="description" value="{{old('description')}}">
+            @error('description')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="thumb">URL immagine</label>
-            <input type="text" id="thumb" name="thumb">
+            <input type="text" id="thumb" name="thumb" value="{{old('thumb')}}">
+            @error('thumb')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="price">Prezzo</label>
-            <input type="text" id="price" name="price">
+            <input type="text" id="price" name="price" value="{{old('price')}}">
+            @error('price')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="series">Serie</label>
-            <input type="text" id="series" name="series">
+            <input type="text" id="series" name="series" value="{{old('series')}}">
+            @error('series')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="sale_date">Data di uscita</label>
-            <input type="date" id="sale_date" name="sale_date">
+            <input type="date" id="sale_date" name="sale_date" value="{{old('sale_date')}}">
+            @error('sale_date')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="type">Tipo</label>
-            <input type="text" id="type" name="type">
+            <input type="text" id="type" name="type" value="{{old('type')}}">
+            @error('type')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <button type="submit">Salva Fumetto</button>
--- laravel-base-crud/resources/views/fumetti/edit.blade.php
+++ laravel-base-crud/resources/views/fumetti/edit.blade.php
@@ -12,36 +12,71 @@
         <div>
             <label for="title">Titolo</label>
             <input type="text" id="title" name="title" value="{{$singoloFumetto->title}}">
+            @error('title')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="description">Descrizione</label>
             <input type="text" id="description" name="description" value="{{$singoloFumetto->description}}">
+            @error('description')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="thumb">URL immagine</label>
             <input type="text" id="thumb" name="thumb" value="{{$singoloFumetto->thumb}}">
+            @error('thumb')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="price">Prezzo</label>
             <input type="text" id="price" name="price" value="{{$singoloFumetto->price}}">
+            @error('price')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="series">Serie</label>
             <input type="text" id="series" name="series" value="{{$singoloFumetto->series}}">
+            @error('series')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="sale_date">Data di uscita</label>
             <input type="date" id="sale_date" name="sale_date" value="{{$singoloFumetto->sale_date}}">
+            @error('sale_date')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <div>
             <label for="type">Tipo</label>
             <input type="text" id="type" name="type" value="{{$singoloFumetto->type}}">
+            @error('type')
+            <div>
+                {{$message}}
+            </div>
+        @enderror
         </div>
 
         <button type="submit">Salva Modifiche</button>
